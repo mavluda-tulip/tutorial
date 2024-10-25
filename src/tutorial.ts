fb6c3730ace0a771d144fe1ccb96c8c9906de92d
mavluda-tulip <mavludakhon.kuchkaralieva@gmail.com>
2024-10-25 18:34:41 +0500

    initial commit

---
+++ src/tutorial.ts
@@ -0,0 +1,19 @@
+
+const books = ["1984", "Deep work", "Atomic Habits"]
+
+let foundBook: string | undefined;
+
+
+for (let book of books) {
+    if (book == "1983") {
+        foundBook = book
+        foundBook.length
+        break;
+    }
+
+}
+
+
+
+
+foundBook?.length
