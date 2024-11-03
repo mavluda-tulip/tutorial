17f071e46ec82c84be56162026cf2ba277e68c8d
mavluda-tulip <mavludakhon.kuchkaralieva@gmail.com>
2024-11-03 22:20:46 +0500

    Done

--- src/tasks.ts
+++ src/tasks.ts
@@ -1,1 +1,64 @@
-console.log("jsjsj")
+const taskForm = document.querySelector<HTMLFormElement>(".form");
+const formInput = document.querySelector<HTMLInputElement>(".form-input");
+
+const taskListElement = document.querySelector<HTMLUListElement>(".list");
+
+type Task = {
+  description: string;
+  isCompleted: boolean;
+};
+
+const tasks: Task[] = loadTask();
+
+tasks.forEach(renderTask);
+
+function loadTask(): Task[] {
+  const storedTasks = localStorage.getItem("tasks");
+  return storedTasks ? JSON.parse(storedTasks) : [];
+}
+
+function createTask(event: SubmitEvent) {
+  event.preventDefault();
+  const taskDescription = formInput?.value;
+  if (taskDescription) {
+    console.log(taskDescription);
+
+    const task: Task = {
+      description: taskDescription,
+      isCompleted: false,
+    };
+
+    addTask(task);
+    renderTask(task);
+    updateStorage();
+    formInput.value = "";
+    return;
+  }
+  alert("Enter");
+}
+
+taskForm?.addEventListener("submit", createTask);
+
+function addTask(task: Task): void {
+  tasks.push(task);
+  console.log(tasks);
+}
+
+function renderTask(task: Task): void {
+  const taskElement = document.createElement("li");
+  taskElement.textContent = task.description;
+  const taskCheckbox = document.createElement("input");
+  taskCheckbox.type = "checkbox";
+  taskCheckbox.checked = task.isCompleted;
+
+  taskCheckbox.addEventListener("change", () => {
+    task.isCompleted = !task.isCompleted;
+    updateStorage();
+  });
+  taskElement.appendChild(taskCheckbox);
+  taskListElement?.appendChild(taskElement);
+}
+
+function updateStorage(): void {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
